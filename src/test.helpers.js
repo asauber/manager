865d07910c3cda045a98eeb73b0fd6925783f0a5
Robert Stires <rstires@linode.com>
2018-01-30 15:11:49 -0500

    Cleanup test.helpers

--- src/test.helpers.js
+++ src/test.helpers.js
@@ -6,73 +6,6 @@ import { fetch } from '~/api/fetch';
 import { state } from '~/data';
 
 
-class InternalAssertionError extends Error {
-  constructor(aVal, bVal, keyPath) {
-    super();
-    const keyPathString = `.${keyPath.join('.')}`;
-    this.message =
-      `Expected 'a' at ${keyPathString} (${JSON.stringify(aVal)} : ${typeof aVal}) ` +
-      `to equal 'b' at ${keyPathString} (${JSON.stringify(bVal)} : ${typeof bVal})`;
-  }
-}
-
-class InternalAssertionTypeError extends Error {
-  constructor(aVal, bVal, keyPath) {
-    super();
-    const keyPathString = `.${keyPath.join('.')}`;
-    this.message =
-      `Expected type of 'a' at ${keyPathString} (${JSON.stringify(aVal)} : ${typeof aVal}) ` +
-      `to equal type of 'b' at ${keyPathString} (${JSON.stringify(bVal)} : ${typeof bVal})`;
-  }
-}
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
-function isObject(o) {
-  return o === Object(o);
-}
-
-export function expectObjectDeepEquals(initialA, initialB, initialPath) {
-  const stackA = [{ a: initialA, path: [initialPath].filter(Boolean) }];
-  const stackB = [initialB];
-
-  while (stackA.length) {
-    const { a, path } = stackA.pop();
-    const b = stackB.pop();
-
-    if (isObject(a) || isObject(b)) {
-      if (!isObject(a) || !isObject(b)) {
-        throw new InternalAssertionTypeError(a, b, path);
-      }
-
-      Object.keys(a).forEach(aKey => {
-        stackA.push({ a: a[aKey], path: [...path, aKey] });
-        stackB.push(b[aKey]);
-      });
-
-      Object.keys(b).forEach(bKey => {
-        if (a[bKey] === undefined && b[bKey] !== undefined) {
-          throw new InternalAssertionError(a[bKey], b[bKey], [...path, bKey]);
-        }
-      });
-    } else if (Array.isArray(a)) {
-      if (!Array.isArray(b)) {
-        throw new InternalAssertionTypeError(a, b, path);
-      } else if (a.length !== b.length) {
-        throw new InternalAssertionError(a, b, path);
-      }
-
-      a.forEach((aVal, i) => {
-        stackA.push({ a: aVal, path: [...path, i] });
-        stackB.push(b[i]);
-      });
-    } else {
-      if (a !== b) {
-        throw new InternalAssertionError(a, b, path);
-      }
-    }
-  }
-}
-
 /**
  * Asserts that a certain kind of request occurs when you invoke the provided
  * function.
@@ -82,8 +15,13 @@ export function expectObjectDeepEquals(initialA, initialB, initialPath) {
  * @param {Object} response - The data that is returned by the fetch call
  * occured to dispatch
  */
-export async function expectRequest(fn, path, expectedRequestData = {}, response = {},
-  fetchStub = null) {
+export async function expectRequest(
+  fn,
+  path,
+  expectedRequestData = {},
+  response = {},
+  fetchStub = null
+) {
   const sandbox = sinon.sandbox.create();
   let checkedRequestData = false;
 
@@ -101,18 +39,16 @@ export async function expectRequest(fn, path, expectedRequestData = {}, response
       });
     }
 
-    const dispatch = sandbox.stub();
-    dispatch.returns(response);
+    const dispatch = jest.fn(() => response);
     await fn(dispatch, () => state);
 
     // This covers the set of API calls that use the thunkFetch helper to make requests.
-    if (isFunction(dispatch.firstCall && dispatch.firstCall.args[0])) {
-      const _dispatch = sandbox.stub();
-      _dispatch.returns(response);
-      await dispatch.firstCall.args[0](_dispatch, () => state);
-      if (_dispatch.callCount === 1 && isFunction(_dispatch.firstCall.args[0])) {
+    if (isFunction(dispatch.mock.calls[0] && dispatch.mock.calls[0][0])) {
+      const _dispatch = jest.fn(() => response);
+      await dispatch.mock.calls[0][0](_dispatch, () => state);
+      if (_dispatch.mock.calls.length === 1 && isFunction(_dispatch.mock.calls[0][0])) {
         await expectRequest(
-          _dispatch.firstCall.args[0], path, expectedRequestData, response, fetchStub);
+          _dispatch.mock.calls[0][0], path, expectedRequestData, response, fetchStub);
         checkedRequestData = true;
       }
     }
@@ -125,7 +61,7 @@ export async function expectRequest(fn, path, expectedRequestData = {}, response
 
       const body = expectedRequestData.body;
       if (body) {
-        Object.keys(body).map(key => expectObjectDeepEquals(requestData[key], body[key], key));
+        Object.keys(body).map(key => expect(requestData[key]).toEqual(body[key]));
       }
 
       checkedRequestData = true;
